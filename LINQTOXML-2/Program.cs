40b06ff35715c316d6b94b4291d12c98f9220093
samikshya <ssamikshya.kar@gmail.com>
2022-02-11 03:18:57 +0530

    Add project files.

---
+++ LINQTOXML-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LINQTOXML_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            XDocument xDocument = new XDocument(
+
+                 new XDeclaration("1.0", "utf-8", "yes"),
+                  new XComment("New XML Document"),
+                  new XElement("Customers",
+                  new XElement("Customer", new XAttribute("ID", "A-101"),
+                  new XElement("First", "Samu"),
+                  new XElement("Second","Samu1"),
+                  new XElement("State", "CTC")
+
+                      ))
+                );
+            xDocument.Save("Customers.xml");
+        }
+    }
+}
